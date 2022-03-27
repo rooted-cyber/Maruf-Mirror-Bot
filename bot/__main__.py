aacf1b24c100b2714bfe711657b7388da0114ad1
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-03-27 19:14:48 +0530

    Add files via upload

--- bot/__main__.py
+++ bot/__main__.py
@@ -62,8 +62,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/harshpreets63/Mirror-Bot")
-    buttons.buildbutton("Channel", "https://t.me/HarshMirror")
+    buttons.buildbutton("💙 OWNER 💚", "https://t.me/rootedcyber")
+    buttons.buildbutton("Group", "https://t.me/mirrorbotgroup")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
